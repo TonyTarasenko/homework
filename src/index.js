fc07763e49eb74da1efc325a601014073de9670c
TonyTarasenko <tonytarasenko.m@gmail.com>
2021-05-22 13:42:25 +0300

    homework 13 revised

--- src/index.js
+++ src/index.js
@@ -16,3 +16,28 @@ function generateList (array, parent = document.body) {
 }
 
 generateList();
+
+const $table = document.createElement('table');
+
+const rows = 10;
+const columns = 10;
+
+for (let i = 0; i < rows; i++) {
+    const $row = $table.insertRow(i);
+
+    for (let j = 1; j <= columns; j++) {
+        const indexOfCell = -1;
+        const $cell = $row.insertCell(indexOfCell);
+        const endOfRow = 10;
+
+        if(j === endOfRow) {
+            const decrementOfCell = 1;
+            $cell.textContent = i + j - (j - decrementOfCell) + '0';
+        } else {
+            $cell.textContent = String(i)+ String(j);
+        }
+    }
+
+}
+
+document.body.append($table);
